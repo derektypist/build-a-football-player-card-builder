4b172a2581dd893e15b8b64d94caf43e881b3eff
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-06-05 10:02:52 +0000

    Updates

--- src.f69400ca.js
+++ src.f69400ca.js
@@ -1791,7 +1791,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45665" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44377" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
